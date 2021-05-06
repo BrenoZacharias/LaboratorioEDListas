6c7d7b145ce24d4158dba14b6c7131ccba1ce62c
breno <breno@DESKTOP-U8621OK>
2021-05-05 21:21:02 -0300

    push

---
+++ src/ex2/EDVetorCircular.java
@@ -0,0 +1,94 @@
+package ex2;
+
+public class EDVetorCircular {
+	Object ed[];
+	int frente;
+	int fim;
+	int tamanho;
+
+	public EDVetorCircular() {
+		ed = new Object[10];
+		frente = -1;
+		fim = -1;
+	}
+
+	public void enfileira(Object obj) {
+		if (!cheia()) {
+			fim++;
+			if (tamanho == 0) {
+				ed[fim] = obj;
+			} else {
+				ed[fim] = obj;
+				frente = fim;
+			}
+			tamanho++;
+		} else {
+			System.out.println("Está cheia");
+		}
+	}
+
+	public Object desenfileira() {
+		if (!vazia()) {
+			int aux = fim;
+			if (tamanho == 2) {
+				fim--;
+				frente = -1;
+			} else {
+				fim--;
+				frente = fim;
+			}
+			tamanho--;
+			return ed[aux];
+		}
+		return "Está vazia";
+	}
+
+	public Object cabeca() {
+		if (!vazia())
+			return ed[0];
+		return "Está vazia";
+	}
+
+	public Object cauda() {
+		if (!vazia())
+			return ed[fim];
+		return "Está vazia";
+	}
+
+	public void mostrarEd() {
+		if (!vazia()) {
+			for (int i = fim; i >= 0; i--) {
+				System.out.println("" + ed[i]);
+			}
+		} else {
+			System.out.println("Está vazia");
+		}
+	}
+
+	public boolean pesquisa(Object obj) {
+		if (!vazia()) {
+			for (int i = 0; i <= fim; i++) {
+				if (toString(ed[i]).equals(toString(obj))) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean vazia() {
+		if (tamanho == 0)
+			return true;
+		return false;
+	}
+
+	public boolean cheia() {
+		if (tamanho == ed.length)
+			return true;
+		return false;
+	}
+
+	public String toString(Object obj) {
+		return obj + "";
+	}
+}
--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -2,4 +2,33 @@ package ex2;
 
 public class Main {
 
+	public static void main(String[] args) {
+		EDVetorCircular ed = new EDVetorCircular();
+
+		System.out.println("esta vazia? " + ed.vazia());
+		System.out.println("esta cheia? " + ed.cheia());
+		ed.enfileira(5);
+		ed.enfileira("i");
+		ed.enfileira(1.6);
+		ed.mostrarEd();
+		System.out.println("desenfileirado " + ed.desenfileira());
+		System.out.println("desenfileirado " + ed.desenfileira());
+		ed.mostrarEd();
+		System.out.println("" + ed.pesquisa("i"));
+		System.out.println("" + ed.pesquisa(5));
+		ed.enfileira("y");
+		ed.enfileira(3);
+		ed.enfileira(44.8);
+		ed.enfileira("tp");
+		ed.enfileira(7.0);
+		ed.enfileira(2);
+		ed.enfileira(88);
+		ed.enfileira(93.1);
+		ed.enfileira("ea");
+		ed.mostrarEd();
+		System.out.println("esta cheia? " + ed.cheia());
+		System.out.println("cabeca = " + ed.cabeca());
+		System.out.println("cauda = " + ed.cauda());
+		System.out.println("esta vazia? " + ed.vazia());
+	}
 }
